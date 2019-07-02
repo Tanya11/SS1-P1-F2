eb0506d2c77020b8222f992fba1b08d94b5ebbe9
Tanya Muhun <Tanyarecinos911@gmail.com>
2019-07-02 11:41:25 -0600

    Update terra.tf

--- Terraform/terra.tf
+++ Terraform/terra.tf
@@ -3,18 +3,18 @@
 
 
 provider "aws" {
-  access_key = ""
-  secret_key = ""
+  access_key = "AKIAYQJDUM5BM4QL3X6S"
+  secret_key = "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"
   region     = "us-east-2"
 }
 
-
-#resource "aws_key_pair" "seminario_Junio2019" {
-#  key_name   = ""
-# public_key = "ssh-rsa root@docker-seminario1"
+#Lo comente para que no vuelva a crear el mismo recurso
+#resource "aws_key_pair" "seminario" {
+#  key_name   = "seminario"
+# public_key = "<PUBLIC-KEY> root@docker-seminario"
 #}
 
-resource "aws_security_group" "rancher_fw_rules-3" {
+resource "aws_security_group" "<NOMBRE-RECURSO-SEGURIDAD>" {
   name        = "rancher_fw_rules3"
   description = "Rancher firewall rules"
 #  vpc_id      = "${aws_vpc.main.id}"
@@ -129,11 +129,11 @@ resource "aws_security_group" "rancher_fw_rules-3" {
 
 
 
-resource "aws_instance" "ubuntu-terraform" {
+resource "aws_instance" "ubuntu-f2" {
 #east-2
   ami           = "ami-0c55b159cbfafe1f0" 
   instance_type = "t2.micro"
-  key_name = "seminario_Junio2019"
+  key_name = "<NOMBRE-DEL-ARCHIV.PEM>"
   root_block_device = {
     volume_type           = "gp2"
     volume_size           = 50
@@ -148,17 +148,17 @@ resource "aws_instance" "ubuntu-terraform" {
       "sudo echo 'Instalacion de Docker'",
       "sudo apt-get -y install docker.io",
       "sudo echo 'Clonacion de images de DockerHub.com'",
-      "sudo docker pull andreeavalos/database:last ",
-      "sudo docker pull andreeavalos/server:last ",
-      "sudo docker pull andreeavalos/api:last ",
+      "sudo docker pull tanyarecinos/semi1bd:last ",
+      "sudo docker pull tanyarecinos/server:last ",
+      "sudo docker pull tanyarecinos/api:last ",
       "sudo echo 'cambio de nombres'",
-      "sudo docker tag andreeavalos/database:last database:latest",
-      "sudo docker tag andreeavalos/server:last server:latest",
-      "sudo docker tag andreeavalos/api:last api:latest",
+      "sudo docker tag tanyarecinos/semi1bd:last semi1bd:latest",
+      "sudo docker tag tanyarecinos/paginasirve:last paginasirve:latest",
+      "sudo docker tag tanyarecinos/api:last api:latest",
       "sudo echo 'Eliminacion de tags innecesarios'",
-      "sudo docker rmi andreeavalos/database:last ",
-      "sudo docker rmi andreeavalos/server:last ",
-      "sudo docker rmi andreeavalos/api:last ",
+      "sudo docker rmi tanyarecinos/semi1bd:last ",
+      "sudo docker rmi tanyarecinos/paginasirve:last ",
+      "sudo docker rmi tanyarecinos/api:last ",
       "sudo echo ''",
       "sudo docker run -it --name api-node api",
       "exit",
@@ -176,14 +176,13 @@ resource "aws_instance" "ubuntu-terraform" {
     }
   }
 
-  depends_on = ["aws_security_group.rancher_fw_rules-3"]
+  depends_on = ["aws_security_group.<NOMBRE-RECURSO-SEGURIDAD>"]
 
-  vpc_security_group_ids = ["${aws_security_group.rancher_fw_rules-3.id}"]
+  vpc_security_group_ids = ["${aws_security_group.<NOMBRE-RECURSO-SEGURIDAD>.id}"]
 
 }
 
 output "ubuntu_ip" {
 #  value = "${aws_eip.ip2.public_ip}"
-  value = "${aws_instance.ubuntu-terraform.public_ip}"
+  value = "${aws_instance.ubuntu-f2.public_ip}"
 }
-
